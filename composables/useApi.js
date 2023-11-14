53000f81ecda1f5d5cc614743445eb9050b47bda
wapmastaz <demolatheophilus8@gmail.com>
2023-11-14 13:16:26 +0100

    added live url

--- composables/useApi.js
+++ composables/useApi.js
@@ -6,7 +6,7 @@ import {useAuth} from '../stores/auth.js'
 export const useApi = () => {
   
   const authStore = useAuth()
-	const baseURL = 'http://mate-africa.test/api/v1/';
+	const baseURL = 'https://authcertify.org.ng/public/api/v1/';
 
 	return axios.create({
 		baseURL,
